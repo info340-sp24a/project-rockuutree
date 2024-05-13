30786438bcc46c7c56d76c3a1d9ca8ff15cd5aed
rockuutree <vurya22@gmail.com>
2024-05-13 16:33:32 -0700

    Ryan

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,22 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/homepage/homepage';
+// import StyleQuizPage from './components/StyleQuizPage';
+// import PaletteAnalysisPage from './components/PaletteAnalysisPage';
+// import StylesPage from './components/StylesPage';
 
 function App() {
   return (
-      <div className="App">
-        <HomePage />
-      </div>
-    );
-  }
-  
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/style-quiz" element={<StyleQuizPage />} />
+        <Route path="/palette-analysis" element={<PaletteAnalysisPage />} />
+        <Route path="/styles" element={<StylesPage />} /> */}
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
