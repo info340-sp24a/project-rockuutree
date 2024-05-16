c8b2625b57287864adb28ebd263c1de059c77a5e
rockuutree <vurya22@gmail.com>
2024-05-16 00:05:31 -0700

    Navbar/footer into jsx

---
+++ my-app/src/components/nav/navScript.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export const useNavigation = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return { isMenuOpen,toggleMenu };
+};
+
