7b23554fe6ebfbc8c45a5a744861ffdcbd1750e1
Carlos Carrillo-Sandoval <carloscs27@Carloss-MacBook-Pro-3.local>
2024-05-17 20:47:15 -0700

    Search bar

--- my-app/src/components/StylePages/StylePage.js
+++ my-app/src/components/StylePages/StylePage.js
@@ -2,26 +2,41 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import NavBar from '../nav/nav';
 import { Footer } from '../footer/footer';
+import SearchBar from './SearchBar';
 import '../../index.css'
 
 import StyleCardList from './StyleCardList';
 
 function StylePage(props){
+    /*Nav Bar */
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
+
+
     const { style_data } = props;
+
+    // Search Bar 
+    const [searchQuery, setSearchQuery] = useState('');
+    const filteredStyleData = style_data.filter(style => 
+        style.Style_Name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
     return(
         <div className='style-page'>
             <NavBar/>
             <main>
-                <StyleCardList style_data={style_data} />
+                <div className='content-container'>
+                    <SearchBar Query={searchQuery} setSearchQuery={setSearchQuery}/>
+                    <StyleCardList style_data={filteredStyleData} />
+                </div>
             </main>
             <Footer />
         </div>
     );
     
 }
+
 
 export default StylePage;
