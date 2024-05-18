4fa1dddfc35581ae9c1173fdf41b738571eb4b4f
ttran005 <ttran005@uw.edu>
2024-05-17 17:20:12 -0700

    StyleQuiz Functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/homepage/homepage';
-// import StyleQuizPage from './components/styles-page/StyleCardList';
 import PaletteAnalysisPage from './components/colorpalette/colorpalette';
 // import StyleQuiz from './components/stylequiz/StyleQuiz';
+import StyleQuizPage from './components/stylequiz/StyleQuiz';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        {/* <Route path="/style-quiz" element={<StyleQuizPage />} /> */}
+      <Route path="/style-quiz" element={<StyleQuizPage />} />
         <Route path="/palette-analysis" element={<PaletteAnalysisPage />} />
+        <Route path="/" element={<HomePage />} />
         {/* <Route path="/styles" element={<StyleQuiz />} /> */}
+
       </Routes>
     </Router>
   );
