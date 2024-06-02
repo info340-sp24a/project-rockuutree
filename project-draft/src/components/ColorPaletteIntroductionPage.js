c5b867462b4e668c3f847211e985156cd929ec9b
Cleo <121153199+cleoreyes@users.noreply.github.com>
2024-06-01 17:46:18 -0700

    responsive, need to retrieve images

--- project-draft/src/components/ColorPaletteIntroductionPage.js
+++ project-draft/src/components/ColorPaletteIntroductionPage.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import seasonalColorWheel from '../assets/season-color-wheel.jpeg';
@@ -9,38 +10,51 @@ import '../css/style.css';
 export default function IntroductionPage(props) {
     return (
       <>
-        <NavBar />
-        <div className="d-flex flex-column text-center quiz-intro w-100 p-0 m-0">
-          <div className="text-white fw-semibold">
-            <p className='fs-2 px-5 py-4'>
-              Find Out Which Colors Reflect Your Unique Style!
-            </p>
+        <NavBar/>
+
+        <main>
+          <div className="d-flex flex-column text-center quiz-intro w-100 pt-5 mt-5">
+            <div div className="text-white fw-semibold">
+              <p className="fs-2 px-5 py-4 pt-2">
+                Find Out Which Colors Reflect Your Unique Style!
+              </p>
+            </div>
+            <div className="d-flex flex-column justify-content-center rounded-5 bg-light text-bubble">
+              <p>
+                Ever wondered which colors suit your personality and style the best?
+              </p>
+              <p>
+                Our Color Palette Analysis Quiz is designed to help you uncover the perfect hues that complement your unique vibe.
+              </p>
+              <p>
+                Whether you're redecorating your home, revamping your wardrobe, or just curious, this quiz will guide you to your ideal color palette.
+              </p>
+            </div>
+            <div className="text-white pt-4 px-5 text-size">
+              <p>
+                Simply answer a few fun and easy questions about your preferences, style, and personality. The quiz takes less than 5 minutes and provides instant results tailored just for you!
+              </p>
+            </div>
+            <div className="py-4 mx-10 px-5">
+              <img className="responsive-image" src={seasonalColorWheel} alt="Seasonal Color Wheel" />
+            </div>
+            <div className="ready-statement text-white pt-4 px-5 text-size">
+              <p>
+                Ready to discover your perfect colors? Click below to start the quiz and find out!
+              </p>
+            </div>
+            <div className="pb-4">
+              <Link to="/palette-analysis/quiz">
+                <button className="fs-3 px-5 py-2 all-buttons rounded">
+                  Begin Quiz
+                </button>
+              </Link>
+            </div>
           </div>
-          <div className="text-bubble mx-5 px-5 py-4 rounded-5">
-            <p>
-              Ever wondered which colors suit your personality and style the best? Our Color Palette Analysis Quiz is designed to help you uncover the perfect hues that complement your unique vibe. Whether you're redecorating your home, revamping your wardrobe, or just curious, this quiz will guide you to your ideal color palette.
-            </p>
-          </div>
-          <div className="text-white pt-4 px-5">
-            <p>
-            Simply answer a few fun and easy questions about your preferences, style, and personality. The quiz takes less than 5 minutes and provides instant results tailored just for you!
-            </p>
-          </div>
-          <div className="py-4">
-            <img src={seasonalColorWheel} />
-          </div>
-          <div className="ready-statement text-white pt-4 fs-5">
-            <p>
-              Ready to discover your perfect colors? Click below to start the quiz and find out!
-            </p>
-          </div>
-          <div className="pb-4">
-            <Link to="/palette-analysis/quiz"><button className="begin-quiz fs-3 px-5 py-2">
-                Begin Quiz
-              </button></Link>
-          </div>
-        </div>
+        </main>
+
         <Footer />
       </>
+
     );
   }
--- project-draft/src/components/ColorPaletteResults.js
+++ project-draft/src/components/ColorPaletteResults.js
@@ -1,6 +1,19 @@
+
 import React from 'react';
-import seasonalColorWheel from '../assets/season-color-wheel.jpeg';
-import { Link } from 'react-router-dom';
+import lightSpring from '../assets/lightspring.png'
+import warmSpring from '../assets/warmspring.png'
+import clearSpring from '../assets/clearspring.png'
+import coolWinter from '../assets/coolwinter.png'
+import clearWinter from '../assets/clearwinter.png'
+import deepWinter from '../assets/deepwinter.png'
+import softAutumn from '../assets/softautumn.png'
+import warmAutumn from '../assets/warmautumn.png'
+import deepAutumn from '../assets/deepautumn.png'
+import coolSummer from '../assets/coolsummer.png'
+import lightSummer from '../assets/lightsummer.png'
+import softSummer from '../assets/softsummer.png'
+
+
 import NavBar from './Nav.js';
 import {Footer} from './Footer.js';
 import '../css/style.css';
@@ -10,11 +23,13 @@ export default function ColorResultsPage(props) {
     const { result } = props;
     return (
       <>
+        <div>
         <NavBar />
         <main>
             <Result result={result}/>
         </main>
         <Footer />
+        </div>
       </>
     )
 }
@@ -24,76 +39,77 @@ function Result(props) {
     const palettes = {
         "LightSpring": {
             name: "Light Spring",
-            image: seasonalColorWheel,
+            image: lightSpring,
             description: "Light Spring palettes are characterized by light, warm, and bright colors. These colors often include pastel shades and delicate hues that reflect the fresh, vibrant feel of springtime."
         },
         "WarmSpring": {
             name: "Warm Spring",
-            image: "",
+            image: warmSpring,
             description: "Warm Spring palettes feature warm and bright colors that exude a sunny and cheerful vibe. These palettes include golden yellows, peachy pinks, and warm greens that are reminiscent of the blooming season."
         },
         "ClearSpring": {
             name: "Clear Spring",
-            image: "",
+            image: clearSpring,
             description: "Clear Spring palettes are known for their bright, warm, and clear colors. These colors are vibrant and have a high contrast, making them ideal for a crisp and lively appearance."
         },
         "CoolWinter": {
             name: "Cool Winter",
-            image: "",
+            image: coolWinter,
             description: "Cool Winter palettes include cool, deep, and vibrant colors. These palettes feature icy blues, deep purples, and stark whites, evoking the crisp, clear essence of winter."
         },
         "ClearWinter": {
             name: "Clear Winter",
-            image: "",
+            image: clearWinter,
             description: "Clear Winter palettes are composed of bright, cool, and highly contrasting colors. These colors are sharp and clear, often including jewel tones and pure hues that reflect the clarity of winter."
         },
         "DeepWinter": {
             name: "Deep Winter",
+            image: deepWinter,
             description: "Deep Winter palettes are characterized by deep, cool, and intense colors. These palettes often include dark blues, rich blacks, and deep reds, embodying the richness and depth of winter."
         },
         "SoftAutumn": {
             name: "Soft Autumn",
-            image: "",
+            image: softAutumn,
             description: "Soft Autumn palettes feature soft, warm, and muted colors. These palettes include earthy tones, gentle browns, and subdued greens, reflecting the mellow and cozy feeling of autumn."
         },
         "WarmAutumn": {
             name: "Warm Autumn",
-            image: "",
+            image: warmAutumn,
             description: "Warm Autumn palettes are known for their warm, rich, and muted colors. These palettes encompass the deep reds, burnt oranges, and golden browns that symbolize the warmth and richness of autumn."
         },
         "DeepAutumn": {
             name: "Deep Autumn",
-            image: "",
+            image: deepAutumn,
             description: "Deep Autumn palettes are comprised of deep, warm, and rich colors. These palettes often include dark olive greens, deep browns, and maroons, capturing the deep and earthy essence of autumn."
         },
         "CoolSummer": {
             name: "Cool Summer",
-            image: "",
+            image: coolSummer,
             description: "Cool Summer palettes include cool, light, and soft colors. These palettes feature pastel blues, soft lavenders, and gentle pinks, reflecting the cool and serene nature of summer."
         },
         "LightSummer": {
             name: "Light Summer",
-            image: "",
+            image: lightSummer,
             description: "Light Summer palettes are characterized by light, cool, and muted colors. These palettes include soft pastel hues, gentle blues, and cool grays, evoking the breezy and soft atmosphere of summer."
         },
         "SoftSummer": {
             name: "Soft Summer",
-            image: "",
+            image: softSummer,
             description: "Soft Summer palettes feature soft, cool, and muted colors. These palettes include muted blues, dusty pinks, and soft greens, capturing the calm and hazy feel of summer."
         }
       }
       const resultData = palettes[result];
     return (
-        <div className="back w-75 shadow-sm d-flex flex-column">
+        <div className="back shadow-sm d-flex flex-column question-box-size shadow-lg">
             <div className="position-relative">
                 <p className="h3 text-center">Result:</p>
                 <p className="h1 text-center">{resultData.name}</p>
             </div>
             <div className="text-center">
-                <img className="img-fluid rounded" src={resultData.image} alt="Question illustration"/>
+                <img className="img-fluid rounded px-3" src={resultData.image} alt="Question illustration"/>
             </div>
             <div className="position-relative">
-                <p className="text-center">{resultData.description}</p>
+                <p className="text-center text-size pt-4 px-2">{resultData.description}</p>
             </div>
         </div>
     )
