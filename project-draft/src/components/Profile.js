715ce91608e7fbb8924d67566ba00f51dc918e2d
Cleo <121153199+cleoreyes@users.noreply.github.com>
2024-06-03 15:35:22 -0700

    added profile colors and displayName change

--- project-draft/src/components/Profile.js
+++ project-draft/src/components/Profile.js
@@ -1,60 +1,35 @@
 // Profile.js
 import React, { useState } from 'react';
 import '../index.css';
-import me from '../assets/mapleMeFace.png';
+import me from '../assets/default_pfp.png';
 import NavBar from './Nav';
 import { getUserProfile, updateUserProfile, addColorCard, addStyleCard } from './User';
-import { getDatabase, ref, child, get as firebaseGet } from "firebase/database";
+import { getDatabase, ref, child, get as firebaseGet, push as firebasePush, onValue} from "firebase/database";
+import { getAuth, updateProfile } from "firebase/auth";
 import { useEffect } from 'react';
 import colorPalettes from "../assets/color_palettes.json"
 
 const Profile = (props) => {
   const { currentUser } = props;
   const [palettes, setPalettes] = useState([]);
+  const [profileColors, setProfileColors] = useState([]);
   const [userProfileState, setUserProfileState] = useState(getUserProfile());
-  const [name, setName] = useState(null);
-  const [email, setEmail] = useState(null);
+  const [newName, setNewName] = useState(null);
+  const [name, setName] = useState(currentUser.displayName);
   const [newColorName, setNewColorName] = useState('');
-  const [newColorDescription, setNewColorDescription] = useState('');
+  const [newHexCode, setNewHexCode] = useState('');
   const [newStyleName, setNewStyleName] = useState('');
   const [newStyleDescription, setNewStyleDescription] = useState('');
 
   const handleSaveProfile = () => {
-    const updatedProfile = {
-      ...userProfileState,
-      name,
-      email,
-    };
-    updateUserProfile(updatedProfile);
-    setUserProfileState(updatedProfile);
-  };
-
-  const handleAddColorCard = () => {
-    const newColor = {
-      name: newColorName,
-      description: newColorDescription,
-    };
-    const updatedColors = addColorCard(newColor);
-    setUserProfileState((prevProfile) => ({
-      ...prevProfile,
-      colors: updatedColors,
-    }));
-    setNewColorName('');
-    setNewColorDescription('');
-  };
-
-  const handleAddStyleCard = () => {
-    const newStyle = {
-      name: newStyleName,
-      description: newStyleDescription,
-    };
-    const updatedStyles = addStyleCard(newStyle);
-    setUserProfileState((prevProfile) => ({
-      ...prevProfile,
-      styles: updatedStyles,
-    }));
-    setNewStyleName('');
-    setNewStyleDescription('');
+    setName(newName)
+    setNewName('');
+    const auth = getAuth();
+    updateProfile(auth.currentUser, {
+      displayName: newName
+      }).catch((error) => {
+        console.error(error);
+      });
   };
 
   // gets the user's palettes from Firebase Realtime Database
@@ -64,34 +39,83 @@ const Profile = (props) => {
     firebaseGet(child(dbRef, `users/${currentUser.uid}/palettes`))
       .then((snapshot) => {
         if (snapshot.exists()) {
+          console.log(snapshot)
           const data = [];
           snapshot.forEach((childSnapshot) => {
+            console.log("child:", childSnapshot)
+            console.log("child key:", childSnapshot.key)
+            console.log("child value:", childSnapshot.val())
             data.push({ key: childSnapshot.key, value: childSnapshot.val() });
           });
           setPalettes(data);
-        } else {
-          console.log("No data available");
         }
       })
       .catch((error) => {
         console.error(error);
       });
   }, [currentUser.uid]);
+ 
+  
+  useEffect(() => {
+    const db = getDatabase();
+    const colorsRef = ref(db, `users/${currentUser.uid}/colors`); //an object of tasks
+    // gets the children of colors key
+    onValue(colorsRef, (snapshot) => {
+      if (snapshot.exists()) {
+        const data = [];
+        snapshot.forEach((childSnapshot) => {
+          console.log("child:", childSnapshot)
+          console.log("child key:", childSnapshot.key)
+          console.log("child value:", childSnapshot.val())
+          data.push({ key: childSnapshot.key, value: childSnapshot.val() });
+        });
+        setProfileColors(data);
+      }
+  })
+  }, []);
 
   const colorPaletteCards = palettes.map(({ key, value }) => {
     const thisColorPalette = colorPalettes[value];
     const gradientString = `linear-gradient(to right, ${thisColorPalette.colors.slice(0, 3).map(color => color.hexCode).join(', ')})`;
     return (
-        <div className="grid-item flex flex-col justify-content-center align-items-center" key={key} style={{ background: gradientString }}>
+      <div className="grid-item flex flex-col justify-content-center align-items-center" key={key} style={{ background: gradientString }}>
+        <div className="flex flex-col justify-content-center align-items-center bg-light bg-gradient rounded px-3 py-2">
+          <div>
+            <p className='fw-bold fs-4 text-center'>{thisColorPalette.name}</p>
+          </div>
+          <p className="text-center">{thisColorPalette.description}</p>
+        </div>
+      </div>
+    );
+  });
+
+  const handleAddColorCard = () => {
+    const db = getDatabase();
+    const colorsRef = ref(db, `users/${currentUser.uid}/colors`); //an object of tasks
+    const result = {[newColorName]: newHexCode};
+    firebasePush(colorsRef, result);
+    setNewColorName('');
+    setNewHexCode('')
+  };
+
+  const colorCards = profileColors.map(({key, value}) => {
+    console.log(key, value);
+    const [colorName, hexCode] = Object.entries(value)[0];
+    return (
+        <div className="grid-item flex flex-col justify-content-center align-items-center" key={key} style={{ background: "#" + hexCode }}>
           <div className="flex flex-col justify-content-center align-items-center bg-light bg-gradient rounded px-3 py-2">
             <div>
-              <p className='fw-bold fs-4 text-center'>{thisColorPalette.name}</p>
+              <p className='fw-bold fs-4 text-center'>{colorName}</p>
             </div>
-              <p className="text-center">{thisColorPalette.description}</p>
           </div>
         </div>
     )
   });
+
+  const handleAddStyleCard = () => {
+    const result = {[newColorName]: newHexCode};
+    firebasePush(result);
+  }
 
   return (
     <>
@@ -101,7 +125,7 @@ const Profile = (props) => {
           <div className="profile-header">
             <img src={me} alt="Profile" className="profile-picture" />
             <div>
-              <h1>{currentUser.displayName}</h1>
+              <h1>{name}</h1>
               <h1>{currentUser.email}</h1>
             </div>
           </div>
@@ -110,15 +134,8 @@ const Profile = (props) => {
             <input
               type="text"
               id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
             />
             <button onClick={handleSaveProfile}>Save Profile</button>
           </div>
@@ -131,14 +148,7 @@ const Profile = (props) => {
           <div className="card-section">
             <h2>Your Colors</h2>
             <div className="card-container">
-              {userProfileState.colors.map((color, index) => (
-                <div key={index} className="card" style={{ backgroundColor: color.hexCode || '#f5f5f5' }}>
-                  <div className="card-content">
-                    <h3>{color.name}</h3>
-                    <p>{color.description}</p>
-                  </div>
-                </div>
-              ))}
+              {colorCards}
             </div>
             <div className="add-card-form">
               <input
@@ -149,11 +159,11 @@ const Profile = (props) => {
               />
               <input
                 type="text"
-                placeholder="Color Description"
-                value={newColorDescription}
-                onChange={(e) => setNewColorDescription(e.target.value)}
+                placeholder="Hex Code: #XXXXXX"
+                value={newHexCode}
+                onChange={(e) => setNewHexCode(e.target.value)}
               />
-              <button onClick={handleAddColorCard}>Save Color Card</button>
+              <button onClick={handleAddColorCard}>Add Color</button>
             </div>
           </div>
           <div className="card-section">
@@ -191,3 +201,4 @@ const Profile = (props) => {
 };
 
 export default Profile;
+
